f259c96ecdd21244ef4e2b9b9af0b1a61c526490
playerony <playerony@gazeta.pl>
2017-01-01 09:33:31 +0100

    Create User entity class

---
+++ src/com/warehouse/entity/User.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author pawel_000
+ */
+
+public class User implements Serializable{
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String place;
+    private String login;
+    private String password;
+    private String rank;
+    
+    public User(){
+        
+    }
+    
+    public User(int id, String login, String password, String rank){
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.rank = rank;
+    }
+    
+    public User(int id, String firstname, String lastname, String login, String password, String place, String rank){
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.login = login;
+        this.password = password;
+        this.place = place;
+        this.rank = rank;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+}
