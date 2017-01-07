c7841a7d49ccec0fd4e781c18d8c097eb2320268
playerony <playerony@gazeta.pl>
2017-01-07 19:53:41 +0100

    Create OrderMenuAbstractController class

---
+++ src/com/warehouse/abstractController/OrderMenuAbstractController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.abstractController;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class OrderMenuAbstractController{
+    public void handleGenerateTXT() {
+        
+    }
+
+    public void handleGeneratePDF() {
+        
+    }
+
+    public void handleInformationAction() {
+        
+    }
+
+    public void handleBackMenuItem() throws IOException {
+        
+    }
+
+    public void handleLogoutMenuItem() throws IOException {
+        
+    }
+}
