67320c0e2e70f02dc7257fcb3b0f7fa7a4f24e66
playerony <playerony@gazeta.pl>
2017-01-08 15:05:22 +0100

    Add tableView for OrderStatusController

--- src/com/warehouse/controller/OrderStatusController.java
+++ src/com/warehouse/controller/OrderStatusController.java
@@ -6,20 +6,31 @@
 package com.warehouse.controller;
 
 import com.warehouse.abstractController.OrderMenuAbstractController;
+import com.warehouse.dao.OrderDao;
 import com.warehouse.entity.Order;
+import com.warehouse.entity.PalleteInfo;
 import com.warehouse.informations.OrderInformations;
+import com.warehouse.utility.AlertBox;
+import com.warehouse.utility.Validate;
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  *
  * @author pawel_000
  */
-public class OrderStatusController extends OrderMenuAbstractController{
+public class OrderStatusController extends OrderMenuAbstractController implements Initializable{
     @FXML
-    public TableView<Order> tableView;
+    public TableView<OrderInformations> tableView;
     @FXML
     public TableColumn<OrderInformations, String> orderID;
     @FXML
@@ -34,6 +45,62 @@ public class OrderStatusController extends OrderMenuAbstractController{
     public TableColumn<OrderInformations, String> clientAddress;
     @FXML
     public TableColumn<OrderInformations, String> whenOrder;
+    
+    private AlertBox alertBox;
+    private OrderDao orderDao;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initInstances();
+        initTableView();
+    }
+    
+    private void initInstances() {
+        alertBox = new AlertBox();
+        orderDao = new OrderDao();
+    }
+    
+    private void initTableView() {
+        orderID.setCellValueFactory(new PropertyValueFactory<>("orderID"));
+        itemName.setCellValueFactory(new PropertyValueFactory<>("itemName"));
+        itemCode.setCellValueFactory(new PropertyValueFactory<>("itemCode"));
+        amount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        clientName.setCellValueFactory(new PropertyValueFactory<>("clientName"));
+        clientAddress.setCellValueFactory(new PropertyValueFactory<>("clientAddress"));
+        whenOrder.setCellValueFactory(new PropertyValueFactory<>("whenOrder"));
+
+        tableView.setItems(getObservableList());
+    }
+    
+    public ObservableList<OrderInformations> getObservableList() {
+        ObservableList<OrderInformations> result = FXCollections.observableArrayList();
+
+        try {
+            Order order = orderDao.getOrderById(1);
+            List<PalleteInfo>palleteInfo = Validate.getPalleteInformations(order.getItems());
+            
+            if (order != null) {
+                for (PalleteInfo p : palleteInfo) {
+                    OrderInformations cm = new OrderInformations();
+                    cm.setOrderID(order.getId());
+                    cm.setClientName(order.getClient().getName());
+                    cm.setClientAddress(order.getClient().getAddress());
+                    cm.setItemName("xDDD");
+                    cm.setItemCode("000");
+                    cm.setAmount(p.getAmount());
+                    cm.setWhenOrder(order.getDate().toString());
+
+                    result.add(cm);
+                }
+            }
+        return result;
+        } catch (Exception e) {
+            alertBox.display(getClass().getSimpleName(), "Cant get informations from DB");
+            e.printStackTrace();
+        }
+
+        return result;
+    }
     
     public void handlePackingOrderAction() throws IOException {
         
