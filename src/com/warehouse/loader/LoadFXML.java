260ece0a909ac8dfd3c894a4e24b307394e85ff5
playerony <playerony@gazeta.pl>
2017-01-11 16:33:38 +0100

    Create getPath method

--- src/com/warehouse/loader/LoadFXML.java
+++ src/com/warehouse/loader/LoadFXML.java
@@ -6,6 +6,7 @@
 package com.warehouse.loader;
 
 import java.io.IOException;
+import java.net.URL;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,13 +17,21 @@ import javafx.stage.Stage;
  * @author pawel_000
  */
 public class LoadFXML {
+    public LoadFXML(){
+        
+    }
+    
     public LoadFXML(final String fxmlFileName) throws IOException {
         loadFile(fxmlFileName);
+    }
+    
+    public URL getPath(final String fileName){
+        return getClass().getResource("/com/warehouse/fxml/" + fileName + ".fxml");
     }
 
     private void loadFile(final String fxmlFileName) throws IOException {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/com/warehouse/fxml/" + fxmlFileName + ".fxml"));
+        Parent root = FXMLLoader.load(getPath(fxmlFileName));
 
         stage.setScene(new Scene(root));
         stage.setResizable(false);
