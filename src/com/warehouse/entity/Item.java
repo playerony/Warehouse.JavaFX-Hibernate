c943f9ec8767ba6bf59d51723b4e5545e0603ba9
playerony <playerony@gazeta.pl>
2017-01-11 10:16:48 +0100

    Create Item entity class

---
+++ src/com/warehouse/entity/Item.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.entity;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class Item {
+    private int id;
+    private String name;
+    private String code;
+    
+    public Item(){
+        
+    }
+    
+    public Item(int id, String name, String code){
+        this.id = id;
+        this.name = name;
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
