8244040b0204e11082fb9838bd92c5c4ee7fac35
playerony <playerony@gazeta.pl>
2017-01-11 15:01:52 +0100

    Create simple cookie class

---
+++ src/com/warehouse/cookie/Cookie.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.cookie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class Cookie {
+    private static Map memory = new HashMap();
+    
+    public static void add(final String where, final String value){
+        if(memory.get(where) != null && memory.get(where).equals(value))
+            memory.replace(where, value);
+        else
+            memory.put(where, value);
+    }
+    
+    public static String get(final String value){
+        return (String) memory.get(value);
+    }
+    
+    public static void remove(final String value){
+        memory.remove(value);
+    }
+    
+    public static void clear(final String value){
+        memory.clear();
+    }
+}
