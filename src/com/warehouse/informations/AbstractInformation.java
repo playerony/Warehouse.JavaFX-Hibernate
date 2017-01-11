5ccc7ecdd9941b8a5d1b695831ec3c49342cdac5
playerony <playerony@gazeta.pl>
2017-01-11 18:53:07 +0100

    Create AbstractInformation class

---
+++ src/com/warehouse/informations/AbstractInformation.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.informations;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class AbstractInformation {
+    private int id;
+    private int amount;
+    
+    private String clientName;
+    private String clientAddress;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientAddress() {
+        return clientAddress;
+    }
+
+    public void setClientAddress(String clientAddress) {
+        this.clientAddress = clientAddress;
+    }
+}
