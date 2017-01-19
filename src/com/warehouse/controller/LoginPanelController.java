dc4513bf02ac7374911611e028350575f960fcad
playerony <playerony@gazeta.pl>
2017-01-19 19:38:30 +0100

    Some changes by login panel

--- src/com/warehouse/controller/LoginPanelController.java
+++ src/com/warehouse/controller/LoginPanelController.java
@@ -5,10 +5,11 @@ import com.warehouse.loader.LoadFXML;
 import com.warehouse.utility.AlertBox;
 import java.io.IOException;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginPanelController {
-    public TextField login;
-    public TextField password;
+    public TextField loginField;
+    public TextField passwordField;
     
     private final AlertBox alertBox;
     private final UserDao userDao;
@@ -20,9 +21,12 @@ public class LoginPanelController {
     }
     
     public void handleButtonClick() throws IOException {
-        if(userDao.find(login.getText(), password.getText()))
+        if(userDao.find(loginField.getText(), passwordField.getText())){
+            Stage stage = (Stage) loginField.getScene().getWindow();
+            stage.close();
+            
             loadFXML = new LoadFXML("checkOrderNumber");
-        else
+        }else
             alertBox.display(getClass().getSimpleName(), "Wrong login or password");
     }
 }
