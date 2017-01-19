d30e0d2ffa80173afacb9b0cf2f8b46acbb6abb8
playerony <playerony@gazeta.pl>
2017-01-19 20:00:52 +0100

    Create Cookie singleton

--- src/com/warehouse/cookie/Cookie.java
+++ src/com/warehouse/cookie/Cookie.java
@@ -13,24 +13,36 @@ import java.util.Map;
  * @author pawel_000
  */
 public class Cookie {
-    private static Map memory = new HashMap();
+    private static Cookie cookie = null;
+    private static Map memory;
     
-    public static void add(final String where, final String value){
+    private Cookie(){
+        memory = new HashMap();
+    }
+    
+    public static synchronized Cookie getInstance(){
+        if(cookie == null)
+            cookie = new Cookie();
+        
+        return cookie;
+    }
+    
+    public synchronized void add(final String where, final String value){
         if(memory.get(where) != null && memory.get(where).equals(value))
             memory.replace(where, value);
         else
             memory.put(where, value);
     }
     
-    public static String get(final String value){
+    public synchronized String get(final String value){
         return (String) memory.get(value);
     }
     
-    public static void remove(final String value){
+    public synchronized void remove(final String value){
         memory.remove(value);
     }
     
-    public static void clear(){
+    public synchronized void clear(){
         memory.clear();
     }
 }
