851bf56a84c5738bd31b0045bcd3425c970f3249
playerony <playerony@gazeta.pl>
2017-02-07 16:36:55 +0100

    Modify PalletsPicked Entity class

--- src/com/warehouse/entity/PalletsPicked.java
+++ src/com/warehouse/entity/PalletsPicked.java
@@ -5,15 +5,35 @@
  */
 package com.warehouse.entity;
 
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author pawel_000
  */
-public class PalletsPicked {
+
+@Entity
+@Table(name="pallets_picked")
+public class PalletsPicked implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
-    private User user;
-    private Client client;
+    
     private String products;
+    
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "worker_id")
+    private User user;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "client_id")
+    private Client client;
     
     public PalletsPicked(){
         
