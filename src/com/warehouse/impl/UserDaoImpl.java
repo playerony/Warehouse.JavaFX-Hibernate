75c7d61de23012c65434f7a25608d22b4aa5e3be
playerony <playerony@gazeta.pl>
2017-02-07 23:01:17 +0100

    Create UserDaoImpl

---
+++ src/com/warehouse/impl/UserDaoImpl.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.impl;
+
+import com.warehouse.dao.UserDao;
+import com.warehouse.entity.User;
+import com.warehouse.service.UserService;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class UserDaoImpl implements UserDao{
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    
+    @Override
+    public boolean find(String name, String password) {
+        List<User> list = UserService.list(sessionFactory);
+        
+        for(User u : list)
+            if(u.getLogin().contains(name) && u.getPassword().contains(password))
+                return true;
+        
+        return false;
+    }
+    
+    @Override
+    public String getUserNameById(int id){
+        return UserService.list(sessionFactory).get(id).getLogin();
+    }
+}
