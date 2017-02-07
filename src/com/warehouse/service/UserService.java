e5cc2777987cc55b73e4b565c60c0ef691b69081
playerony <playerony@gazeta.pl>
2017-02-07 22:44:20 +0100

    Create UserService

---
+++ src/com/warehouse/service/UserService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.service;
+
+import com.warehouse.entity.Item;
+import com.warehouse.entity.User;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class UserService {
+    public static List<User> list(SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        List<User> list = session.createQuery("from User").list();
+        session.close();
+        
+        return list;
+    }
+}
