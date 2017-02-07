ee1e2de71b4ff8bd823527d86b86b508c5dfe216
playerony <playerony@gazeta.pl>
2017-02-07 22:44:41 +0100

    Create PackingService

---
+++ src/com/warehouse/service/PackingService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.warehouse.service;
+
+import com.warehouse.entity.Item;
+import com.warehouse.entity.PalletsPacked;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class PackingService {
+    public static List<PalletsPacked> list(SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        List<PalletsPacked> list = session.createQuery("from PalletsPacked").list();
+        session.close();
+        
+        return list;
+    }
+}
